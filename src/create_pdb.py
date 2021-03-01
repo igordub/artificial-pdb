cce7df56726465b8719d7c550711257cf53036df
Igors Dubanevics <igors.dubanevics@york.ac.uk>
2021-03-01 10:39:16 +0200

    CreatPython script for PDB making

---
+++ src/create_pdb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+This script downloads the data needed for analysis from the ONS
+API. It uses series that are in the yaml config file.
+"""
+
+from biopandas.pdb import PandasPdb
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Master function
+    create_pdb()
